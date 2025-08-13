cfe430362f61cd44a30a081f14d5177829c0b4aa
ryanmustari <ryan@viasocial.io>
2025-08-13 11:24:07 -0700

    added variable names to remote infra

---
+++ infra/remote/variables.tf
@@ -0,0 +1,20 @@
+variable "bucket_name" {
+  type = string
+}
+
+variable "db_name" {
+  type = string
+}
+
+variable "db_user" {
+  type = string
+}
+
+variable "db_password" {
+  type = string
+}
+
+variable "db_port" {
+  type = integer
+  default = 5432
+}
